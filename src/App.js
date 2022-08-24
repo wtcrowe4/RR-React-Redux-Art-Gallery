2ba3232ee3be1b1285029e299411f8c63b15d07d
Thomas Crowe <wcrowe@g.clemson.edu>
2022-08-24 18:47:16 -0400

    moved dark mode activity files

--- src/App.js
+++ src/App.js
@@ -57,19 +57,4 @@ export default connect(mapStateToProps)(App);
 
 
 
-// import Nav from './components/Nav'
-// import ContentWrapper from './components/ContentWrapper'
-// import Footer from './components/Footer'
 
-// function App() {
-  
-//   return (
-//     <div style={{ backgroundColor: 'white', color: 'black' }} className="App">
-//       <Nav />
-//       <ContentWrapper />
-//       <Footer />
-//     </div>
-//   );
-// }
-
-// export default App;
